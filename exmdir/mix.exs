d8bd30b31bc92edc292906333b490dca9cbd18e4
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2019-03-22 14:26:07 +0900

    add  coveralls tool

--- exmdir/mix.exs
+++ exmdir/mix.exs
@@ -8,6 +8,8 @@ defmodule Exmdir.MixProject do
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       escript: escript()
     ]
   end
@@ -22,7 +24,8 @@ defmodule Exmdir.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:ex_doc, only: :dev, runtime: false}
+      {:ex_doc, only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
