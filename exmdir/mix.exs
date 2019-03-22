c9c1ce9293a9e8e6dfd66f2e85ed627919cde7db
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2019-03-22 15:12:09 +0900

    add ex_doc config

--- exmdir/mix.exs
+++ exmdir/mix.exs
@@ -9,8 +9,18 @@ defmodule Exmdir.MixProject do
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
-      escript: escript()
+      # preferred_cli_env: ["coveralls": :test,
+      #  "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      escript: escript(),
+      # Docs
+      name: "ExMDir",
+      source_url: "https://gitlab.com/carryprotocol/hongseungwoo-ls",
+      homepage_url: "https://gitlab.com/carryprotocol/hongseungwoo-ls",
+      docs: [
+        main: "ExMDir", # The main page in the docs
+        logo: "media/logo.png",
+        extras: ["README.md"]
+      ]
     ]
   end
 
@@ -26,6 +36,10 @@ defmodule Exmdir.MixProject do
     [
       {:ex_doc, only: :dev, runtime: false},
       {:excoveralls, "~> 0.10", only: :test},
+      {:stream_data, "~> 0.4.2", only: [:dev, :test], runtime: false},
+      {:credo, "~> 0.9.0-rc1", only: :dev, runtime: false},
+      {:mix_test_watch, "~> 0.5", only: :dev, runtime: false},
+      {:dialyxir, "~> 0.5", only: :dev, runtime: false}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
