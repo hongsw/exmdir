dbf01ff369cff9a0383805db900846ef53aa9a54
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2019-03-22 19:01:10 +0900

    Add tests and add config

--- test/exmdir_test.exs
+++ test/exmdir_test.exs
@@ -4,12 +4,17 @@ defmodule ExmdirTest do
   alias Exmdir.CLI
   doctest Mix.Tasks.Exmdir
 
-  test "Test --help" do
-    assert ["--help"] |> CLI.parse_args |> CLI.response == "usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]"
+  describe "parse_args should parse args" do
+    test "Test --help" do
+      assert ["--help"] |> CLI.parse_args |> CLI.response == "usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]"
+    end
+    test "Should fail" do
+      assert false
+    end
   end
 
   test "formatting" do
     assert CLI.formatting(%{}) == "%{}"
-  end  
+  end
 end
 
