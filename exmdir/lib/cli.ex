58454a3483d8dd16046af0a92d2108d7c3f557a0
Seungwoo hong <1100974+hongsw@users.noreply.github.com>
2019-03-22 18:10:15 +0900

    fix #7 make feature '-l' option

--- exmdir/lib/cli.ex
+++ exmdir/lib/cli.ex
@@ -18,7 +18,7 @@ defmodule Exmdir.CLI do
     args
     |> parse_args
     |> response
-    |> IO.puts
+    |> stdout
     System.halt(0)
   end
 
@@ -29,17 +29,37 @@ defmodule Exmdir.CLI do
     Logger.info(inspect({opts, List.to_string(word)}) )
     {opts, List.to_string(word)}
   end
+  def stdout(rsp) do
+    cond do 
+      String.valid?(rsp) == true-> 
+        IO.puts(rsp)
+      String.valid?(rsp) == false-> 
+        rsp |> Enum.all?(fn x -> IO.puts(inspect(x)) end)
+    end
+  end
 
+  def formatting(fileObject) do
+    Kernel.inspect(fileObject)
+  end
   def response({opts, word}) do
     cond do 
       opts[:halt] == true -> 
         System.halt(1)
       opts[:help] == true -> 
         "usage: ls [-ABCFGHLOPRSTUWabcdefghiklmnopqrstuwx1] [file ...]"
-      word == "" ->
-        File.ls!() |> Enum.all?(fn x -> IO.puts(x) end)
+      word == "" and Keyword.has_key?(opts, :l) == false ->
+        File.ls!() 
+      opts[:l] == true -> 
+        File.ls!() |> Enum.map(fn x -> fileObject(x) end)
+        
       # true ->
       #   IO.puts('true')
+    end
+  end
+  def fileObject(filename) do
+    case File.stat filename do
+      {:ok, stat} -> %{status: :ok, atime: stat.atime, size: stat.size, filename: filename}
+      {:error, reason} -> %{status: :error, filename: filename}
     end
   end
 
